aada773c94e2417d2a759f65b5430abf43e93996
Kelvin <kelvin-798@hotmail.com>
2026-07-19 01:19:40 -0300

    Updating cors frontend domain

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://maintenance-prediction-ai-eight.vercel.app"
+        "https://maintenance-prediction-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
